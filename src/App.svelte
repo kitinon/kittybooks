ef3ec9e5cd6790d2048ae280a39ed01382986496
Kitinon Wangpattanamongkol <kitinon@hiqfood.com>
2020-02-26 11:33:32 +0700

    Initital commit

---
+++ src/App.svelte
@@ -0,0 +1,85 @@
+<script>
+  import books from './bookdata.js'
+</script>
+
+<style>
+  .storetitle {
+    display: flex;
+    margin-bottom: 20px
+  }
+
+  .storetitle > img {
+    height: 80px;
+  }
+
+  .bookshelf {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+    grid-gap: 32px;
+    grid-auto-flow: dense
+  }
+
+  .bookholder {
+    display: flex;
+    justify-content: flex-start
+  }
+
+  .bookimage {
+    width: 160px;
+    margin-right: 15px
+  }
+  .bookimage > img { width: 100% }
+
+  .bookinfo {
+    display: grid;
+    width: 100%;
+    grid-template-columns: min-content 1fr;
+    grid-auto-rows: min-content;
+    font-size: 11.5pt
+  }
+  
+  .bookinfo > nobr {
+    margin-right: 10px;
+    color: gray
+  }
+
+  .booktitle {
+    grid-column: 1 / -1;
+    margin-bottom: 5px
+  }
+</style>
+
+<div class="storetitle">
+  <img src="/images/kitty.jpg" alt="kitty logo">
+  <h1>ร้านคิตตี้บุ๊ค</h1>
+</div>
+<div class="bookshelf">
+  {#each books as book}
+    <div class="bookholder">
+      <div class="bookimage">
+        <img src={`/images/${book.id}.jpg`}
+             alt={book.title} title={book.title}>
+      </div>
+      <div class="bookinfo">
+        <span class="booktitle"><b>{book.title}</b></span>
+        <nobr>บาร์โค้ด</nobr>{book.id}
+        <nobr>สำนักพิมพ์</nobr>{book.publisher}
+        <nobr>ผู้เขียน</nobr>{book.author}
+        <nobr>ราคา</nobr>{book.price} บาท
+      </div>            
+    </div>
+  {/each}
+</div>
+<!--
+.bookshelf {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+    grid-gap: 32px;
+    grid-auto-flow: dense;
+}
+.bookshelf {
+  display: flex;
+  flex-wrap: wrap;
+  background-color: darkgray
+}
+-->
---
+++ src/bookdata.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    "id" : "9786162624964",
+    "title" : "พัฒนาเว็บและเพิ่มลูกเล่นด้วย JavaScript",
+    "author" : "สุพจน์ สง่ากอง",
+    "publisher" : "Simplify ซิมพลิฟาย",
+    "price" : 199
+  },
+  {
+    "id" : "9786160834938",
+    "title" : "สร้างเว็บไซต์แบบ Responsive ด้วย Bootstrap ร่วมกับ CSS และ JavaScript",
+    "author" : "บัญชา ปะสีละเตสัง",
+    "publisher" : "ซีเอ็ดยูเคชั่น",
+    "price" : 380
+  },
+  {
+    "id" : "8859165001191",
+    "title" : "พัฒนาเว็บแอพด้วย JavaScript แบบ MEAN Stack",
+    "author" : "ศุภชัย สมพานิช",
+    "publisher" : "สวัสดี ไอที",
+    "price" : 229
+  },
+  {
+    "id" : "9786160825394",
+    "title" : "พัฒนาเว็บแอบพลิเคชั่นด้วย JavaScript",
+    "author" : "จตุรพัชร์ พัฒนทรงศิวิไล",
+    "publisher" : "ซีเอ็ดยูเคชั่น",
+    "price" : 290
+  },
+  {
+    "id" : "9786162620706",
+    "title" : "สร้างเว็บและเพิ่มลูกเล่นด้วย JavaScript DHTML ฉบับสมบูรณ์",
+    "author" : "ประชา พฤกษ์ประเสริฐ",
+    "publisher" : "Simplify ซิมพลิฟาย",
+    "price" : 199
+  },
+  {
+    "id" : "9786162625305",
+    "title" : "พัฒนาเว็บแอพพลิเคชันด้วย Node.js Express+MongoDB",
+    "author" : "จีราวุธ วารินทร์",
+    "publisher" : "Simplify ซิมพลิฟาย",
+    "price" : 229
+  }
+]
