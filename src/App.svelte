c053fd0b2a98204bb05c3b9303bcbdc58f0426d7
Kitinon Wangpattanamongkol <kitinon@hiqfood.com>
2020-02-27 13:04:32 +0700

    refactor into smaller components.

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,16 @@
 <script>
   import books from './bookdata.js'
+  import StoreTitle from './StoreTitle.svelte'
+  import Book from './Book.svelte'
 </script>
 
 <style>
-  .storetitle {
-    display: flex;
-    margin-bottom: 20px
-  }
-
-  .storetitle > img {
-    height: 80px;
-  }
 
   .bookshelf {
     display: grid;
     grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
     grid-auto-flow: dense
   }
-
   /* Single column display for phones */
   @media (max-width: 459px) {
     .bookshelf {
@@ -26,56 +19,11 @@
     }
   }
 
-  .bookholder {
-    display: flex;
-    justify-content: flex-start;
-    margin: 1% 2% 5% 1%
-  }
-
-  .bookimage {
-    width: 160px;
-    margin-right: 15px
-  }
-  .bookimage > img { width: 100% }
-
-  .bookinfo {
-    display: grid;
-    width: 100%;
-    grid-template-columns: min-content 1fr;
-    grid-auto-rows: min-content;
-    font-size: 11.5pt
-  }
-  
-  .bookinfo > span {
-    white-space: nowrap;
-    margin-right: 10px;
-    color: gray
-  }
-
-  .booktitle {
-    grid-column: 1 / -1;
-    margin-bottom: 5px
-  }
 </style>
 
-<div class="storetitle">
-  <img src="/images/kitty.jpg" alt="kitty logo">
-  <h1>ร้านคิตตี้บุ๊ค</h1>
-</div>
+<StoreTitle/>
 <div class="bookshelf">
   {#each books as book}
-    <div class="bookholder">
-      <div class="bookimage">
-        <img src={`/images/${book.id}.jpg`}
-             alt={book.title} title={book.title}>
-      </div>
-      <div class="bookinfo">
-        <div class="booktitle"><b>{book.title}</b></div>
-        <span>บาร์โค้ด</span>{book.id}
-        <span>สำนักพิมพ์</span>{book.publisher}
-        <span>ผู้เขียน</span>{book.author}
-        <span>ราคา</span>{book.price} บาท
-      </div>            
-    </div>
+    <Book data={book}/>
   {/each}
 </div>
---
+++ src/BookInfo.svelte
@@ -0,0 +1,32 @@
+<script>
+  export let data
+</script>
+
+<style>
+  .container {
+    display: grid;
+    width: 100%;
+    grid-template-columns: min-content 1fr;
+    grid-auto-rows: min-content;
+    font-size: 11.5pt
+  }
+  
+  .container > span {
+    white-space: nowrap;
+    margin-right: 10px;
+    color: gray
+  }
+
+  .title {
+    grid-column: 1 / -1;
+    margin-bottom: 5px
+  }
+</style>
+
+<div class="container">
+  <div class="title"><b>{data.title}</b></div>
+  <span>บาร์โค้ด</span>{data.id}
+  <span>สำนักพิมพ์</span>{data.publisher}
+  <span>ผู้เขียน</span>{data.author}
+  <span>ราคา</span>{data.price} บาท
+</div>            
